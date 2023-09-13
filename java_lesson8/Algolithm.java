e32b768130daf6642f373c6d2c2184935b5321d5
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-13 20:16:21 +0900

    [add] array lesson

---
+++ java_lesson8/Algolithm.java
@@ -0,0 +1,11 @@
+package java_lesson8;
+
+class Algolithm {
+  public void arrayLesson() {
+    int[] array = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.println(array[i]);
+    }
+  }
+}
---
+++ java_lesson8/Main.java
@@ -0,0 +1,9 @@
+package java_lesson8;
+
+public class Main {
+  public static void main(String[] args) {
+    Algolithm algolithm = new Algolithm();
+
+    algolithm.arrayLesson();
+  }
+}
