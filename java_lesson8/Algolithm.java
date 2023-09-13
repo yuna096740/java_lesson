eac9e4744dfd732961056f4ea0a46f406f8d194c
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-13 20:22:33 +0900

    [add] array lesson *2

--- java_lesson8/Algolithm.java
+++ java_lesson8/Algolithm.java
@@ -1,10 +1,23 @@
 package java_lesson8;
 
 class Algolithm {
+
   public void arrayLesson() {
     int[] array = { 1, 2, 3, 4, 5 };
 
     for (int i = 0; i < array.length; i++) {
+      System.out.println(array[i]);
+    }
+  }
+
+  public void arrayLesson2() {
+    int[] array = {1,2,3,4,5};
+
+    for (int i = 0; i <= 4; i++) {
+      array[i] *= 2;
+    }
+
+    for (int i = 0; i < array.length; i++ ) {
       System.out.println(array[i]);
     }
   }
